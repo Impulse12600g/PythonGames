1f35582d91f3248541ad34a5db71a39406a92e9a
Impulse12600g <k.lal.1078780@gmail.com>
2023-09-25 18:40:33 -0600

    Begin scrolling background

--- flappy_bird_1.py
+++ flappy_bird_1.py
@@ -5,6 +5,9 @@ from pygame.locals import *
 def flappy():
     pygame.init()
 
+    clock = pygame.time.Clock
+    fps = 60
+
     screen_width = 864
     scree_height = 936
     # initial screen params
@@ -13,13 +16,31 @@ def flappy():
     # window title
     pygame.display.set_caption('Flappy Bird')
 
+    # define game variables
+    ground_scroll = 0
+    scroll_speed = 4
+
+    # load in image
+    bg = pygame.image.load('img/bg.png')
+    ground_img = pygame.image.load('img/ground.png')
     run = True
 
     # initial game loop
     while run:
 
+        clock.tick(fps)  # scrolls a lot slower
+
+        # draw background
+        screen.blit(bg, (0, 0))
+
+        # drae and scroll ground
+        screen.blit(ground_img, (ground_scroll, 768))
+        ground_scroll -= scroll_speed
+
         for event in pygame.event.get():  # get all events that happening
             if event.type == pygame.QUIT:  # looking for specific one
                 run = False  # breaks loop
+
+        pygame.display.update()
 
     pygame.quit()
